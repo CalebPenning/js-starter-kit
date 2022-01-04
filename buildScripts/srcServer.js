87fa334d002d9c972b33261d912567a0c8f425a4
calebpenning <calebpenning@mail.com>
2022-01-03 22:18:50 -0800

    added some fetch logic

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -18,6 +18,14 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../src/index.html"))
 })
 
+app.get('/users', (req, res) => {
+  res.json([
+    {id: 1, firstName: "Bob", lastName: "Smith", email: "bobsmith@yahoo.com"},
+    {id: 2, firstName: "Cob", lastName: "Corn", email: "cobcornsmith@yahoo.com"},
+    {id: 3, firstName: "Blob", lastName: "Smirth", email: "blobsmirth@yahoo.com"}
+  ])
+})
+
 app.listen(PORT, (err) => {
   if (err) console.log(err)
   else open(`http://localhost:${PORT}`)
