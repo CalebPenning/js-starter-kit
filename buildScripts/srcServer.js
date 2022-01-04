28fde73e97fccb5f7e58dc0f7fd187c0e2668960
calebpenning <calebpenning@mail.com>
2022-01-03 18:32:07 -0800

    configured babel and webpack for use of es6 modules without modifying package.json

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -1,9 +1,16 @@
-const express = require("express")
-const path = require("path")
-const open = require("open")
+import express from "express"
+import path from "path"
+import open from "open"
+import webpack from "webpack"
+import config from "../webpack.config.dev.js"
 
 const PORT = 3000
 const app = express()
+const compiler = webpack(config)
+
+app.use(require('webpack-dev-middleware') (compiler, {
+  publicPath: config.output.publicPath
+}))
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../src/index.html"))
