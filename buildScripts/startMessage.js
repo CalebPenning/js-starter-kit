ef669ad22af5b8c974d7aecc8d74ffdf407d8b93
calebpenning <calebpenning@mail.com>
2022-01-03 19:23:17 -0800

    added eslintrc file and removed or edited all console.log statements

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,3 +1,3 @@
 import chalk from "chalk"
 
-console.log(chalk.green("Starting application in development mode..."))
+console.log(chalk.green("Starting application in development mode...")) // eslint-disable-line no-console
