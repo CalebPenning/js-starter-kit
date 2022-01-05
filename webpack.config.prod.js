6e188c181b06ea659c836b22b643461b5686b670
calebpenning <calebpenning@mail.com>
2022-01-04 23:03:49 -0800

    dist build commands are working including bundling, html building, and teardown

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,5 +1,6 @@
 import path from "path";
 import HtmlWebpackPlugin from "html-webpack-plugin"
+import MiniCssExtractPlugin from "mini-css-extract-plugin"
 
 export default {
   mode: "production",
@@ -11,15 +12,18 @@ export default {
   output: {
     path: path.resolve(__dirname, "dist"),
     publicPath: "/",
-    filename: "[name].js"
+    filename: "[name].[chunkhash].js"
   },
   plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].[chunkhash].css"
+    }),
     new HtmlWebpackPlugin({ template: "./src/index.html" }),
   ],
   module: {
     rules: [
       { test: /\.js$/, exclude: /node_modules/, use: ["babel-loader"] },
-      { test: /\.css$/, use: ["style-loader", "css-loader"] },
+      { test: /\.css$/, use: [MiniCssExtractPlugin.loader, "css-loader"] },
     ],
   },
 };
